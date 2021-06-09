e43437559ec9d989f719b48100bdfa3d60773abf
kannahs <bp394007@gmail.com>
2021-06-09 18:45:44 +0700

    first commit

---
+++ lab8_php_database/header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+	<meta charset="UTF-8"> 
+	<link href="style.css" rel="stylesheet" type="text/stylesheet" media="screen" /> 
+	<style>
+		.header{
+			display: block;
+			background-color: #1f5faa;
+			color: white;
+			width: 100%;
+			position: fixed;
+			top: 0;
+			left: 0;
+			margin: 0;
+			padding: 0px 0px 0px 20px;
+			text-align: left;
+			letter-spacing: 4px;
+		}
+		a{
+			display: inline;
+		}
+		nav a {
+		padding: 10px 30px;
+		display: inline-block;
+		color: #ffffff;
+		font-size: 14px;
+		text-decoration: none;
+		font-weight: bold;
+		}
+		nav a.active,
+		nav a:hover {
+		background-color: #008b8b;
+		}
+		header h1 {
+			margin: 0px -20px;
+			padding: 10px;
+			background-color: #ffffff;
+			color: black;
+		}
+		body {
+		line-height: 1;
+		font-size: 100%;
+		font-family:'Open sans', sans-serif;
+		color: #00000;
+		}
+		
+	</style>
+</head> 
+<body> 
+	<div class="header">
+	<header>
+		<h1>Database</h1>
+	</header>	
+	<nav>
+	<a href="home.html" class="active">Home</a>
+	<a href="about.html">About</a>
+	<a href="kontak.html">Kontak</a>
+	</nav>
+	</div>
